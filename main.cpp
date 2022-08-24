d7036040d451d56b08335ea8b9236f1546f48f61
Alexey <atunkin64@icloud.com>
2022-08-24 08:46:47 +0300

    init comit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "json_converter.hpp"
+
+int main(){
+
+    //JSON json;                    
+    //json.parse("{ "key" : val, ..."); //convert text representation into JSON class instance
+    //json["KEY"] = newValue            //Change some setting
+    //std::string config = json.dump(); //make valid representation of the current config in string format
+
+    return 0;
+}
